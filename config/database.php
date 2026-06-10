0b40e3a4d97c863879fd99f30e87e2946fb3d2c6
Nikita Minaev <catvoodoo0@gmail.com>
2026-06-10 12:37:06 +0000

    Обновил database.php кредиты

--- config/database.php
+++ config/database.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 return [
     'driver' => 'mysql',
-    'host' => getenv('DB_HOST') ?: 'mysql',
+    'host' => getenv('DB_HOST') ?: 'mariadb',
     'port' => getenv('DB_PORT') ?: '3306',
-    'database' => getenv('DB_DATABASE') ?: 'template',
-    'username' => getenv('DB_USERNAME') ?: 'template',
-    'password' => getenv('DB_PASSWORD') ?: 'template',
+    'database' => getenv('DB_DATABASE') ?: 'rchsnd',
+    'username' => getenv('DB_USERNAME') ?: 'rchsnd',
+    'password' => getenv('DB_PASSWORD') ?: 'rchsnd',
     'charset' => 'utf8mb4',
 ];
